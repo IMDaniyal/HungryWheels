77f1583274ea9fd497ae0d68c6df1b687888d392
IMDaniyal <daniyalakhlaq1@gmail.com>
2019-04-19 17:57:27 +0500

    Just made login and theme changed... still working on social media.

--- app/src/main/java/com/example/hungrywheels/login_activity.java
+++ app/src/main/java/com/example/hungrywheels/login_activity.java
@@ -3,11 +3,12 @@ package com.example.hungrywheels;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        getSupportFragmentManager().beginTransaction().replace(R.id.placeholder,new login_fragment()).commit();
     }
 }
