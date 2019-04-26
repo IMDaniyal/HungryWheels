34c34225c7de23b1776537e9bb34f468a494c5ce
nomi1315 <l164227@lhr.nu.edu.pk>
2019-04-26 19:53:18 +0500

    Nomi's Mic

--- app/src/main/java/com/example/hungrywheels/login_fragment.java
+++ app/src/main/java/com/example/hungrywheels/login_fragment.java
@@ -1,16 +1,25 @@
 package com.example.hungrywheels;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -20,21 +29,20 @@ public class login_fragment extends Fragment {
 
     TextView username;
     TextView password;
-
+    private int req_code = 1111;
+    private ImageButton btn_mic;
 
     public login_fragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onAttach(Context context)
-    {
+    public void onAttach(Context context) {
         super.onAttach(context);
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
@@ -43,20 +51,27 @@ public class login_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.login_fragment, container, false);
+        View v = inflater.inflate(R.layout.login_fragment, container, false);
         TextView register = v.findViewById(R.id.registertxt);
 
-        username= v.findViewById(R.id.nametxt);
-        password=v.findViewById(R.id.passwordtxt);
+        username = v.findViewById(R.id.nametxt);
+        password = v.findViewById(R.id.passwordtxt);
+        btn_mic = v.findViewById(R.id.imageButton);
+
+        btn_mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                letstalk();
+            }
+        });
 
         Button btn = v.findViewById(R.id.loginbutton);
         btn.setOnClickListener(new Button.OnClickListener() {
                                    @Override
-                                   public void onClick(View view)
-                                   {
+                                   public void onClick(View view) {
 
 
-                                       LoginThread alpha=new LoginThread(getActivity(),username.getText().toString(),password.getText().toString());
+                                       LoginThread alpha = new LoginThread(getActivity(), username.getText().toString(), password.getText().toString());
                                        alpha.execute();
 
 
@@ -66,27 +81,24 @@ public class login_fragment extends Fragment {
 
         Button btn2 = v.findViewById(R.id.otherlogin);
         btn2.setOnClickListener(new Button.OnClickListener() {
-                                   @Override
-                                   public void onClick(View view)
-                                   {
+                                    @Override
+                                    public void onClick(View view) {
 
 
-                                       Intent i= new Intent(getActivity(), MainActivity.class);
-                                       startActivity(i);
+                                        Intent i = new Intent(getActivity(), MainActivity.class);
+                                        startActivity(i);
 
-                                   }
-                               }
+                                    }
+                                }
         );
 
 
-        register.setOnClickListener(new TextView.OnClickListener()
-                                    {
+        register.setOnClickListener(new TextView.OnClickListener() {
                                         @Override
-                                        public void onClick(View view)
-                                        {
+                                        public void onClick(View view) {
 
 
-                                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.placeholder,new registration_fragment()).commit();
+                                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new registration_fragment()).commit();
 
                                         }
                                     }
@@ -96,4 +108,32 @@ public class login_fragment extends Fragment {
 
     }
 
+    private void letstalk() {
+
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Tell us your Username:");
+
+        try {
+            startActivityForResult(intent, req_code);
+        } catch (ActivityNotFoundException e) {
+        }
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == req_code) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> voice_in_text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                username.setText(voice_in_text.get(0));
+            }
+        }
+    }
 }
