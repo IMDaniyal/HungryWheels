e4fa3f2f57299b2a641554fa4208b61684a016de
IMDaniyal <daniyalakhlaq1@gmail.com>
2019-05-02 12:46:16 +0500

    Profile picture offline

--- app/src/main/java/com/example/hungrywheels/OrderNowScreen.java
+++ app/src/main/java/com/example/hungrywheels/OrderNowScreen.java
@@ -7,17 +7,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.io.Serializable;
 
 public class OrderNowScreen extends AppCompatActivity {
 
-
+    private AdView mAdView;
+    int REQUEST_INVITE=10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_now_screen);
 
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
         TextView item=findViewById(R.id.itemname);
         TextView res=findViewById(R.id.Rname);
         TextView price=findViewById(R.id.pricetag);
